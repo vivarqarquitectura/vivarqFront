99fc0513ba6a1c720059dd76d1e21e490a29c0e8
mmogermankohan <mmogermankohan@gmail.com>
2025-08-08 12:25:53 -0300

    Se modifico la sección de PASOS a un modo mas simple

---
+++ src/components/seccion2/TarjetaPasos.jsx
@@ -0,0 +1,29 @@
+const TarjetaPasos = () => {
+    return (
+        <div className="tarjetaPasos">
+                <div className="contenidoPasos">
+                        <p>01</p>
+                    <div className="contenidoPasosTexto">
+                        <h1>Planos con medidas</h1>
+                        <p>Planos con medidas y especificaciones técnicas que permitirá a los obreros obtener la información necesaria para la construcción de tu obra.</p>
+                    </div>
+                </div>
+                <div className="contenidoPasos">
+                    <p>02</p>
+                    <div className="contenidoPasosTexto">
+                        <h1>Imágenes en 3D</h1>
+                        <p>Imágenes y videos de tu futura obra con todos los detalles y gustos a medida para explorar opciones posibles</p>
+                    </div>
+                </div>
+                <div className="contenidoPasos">
+                    <p>03</p>
+                    <div className="contenidoPasosTexto">
+                        <h1>Materiales necesarios</h1>
+                        <p>Lista de materiales necesarios para la construcción de la obra, especificado por materiales y sus cantidades como así también sus respectivas dimensiones.</p>
+                    </div>
+                </div>
+        </div>
+    )
+}
+
+export default TarjetaPasos;
