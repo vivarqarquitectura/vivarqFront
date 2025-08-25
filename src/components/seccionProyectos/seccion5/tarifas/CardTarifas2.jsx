d583a8cffd50ef7a3785baca53765cde86d3e9ad
mmogermankohan <mmogermankohan@gmail.com>
2025-08-25 10:42:38 -0300

    Se añadio la referencia de 'mas popular' a las tarjetas de opciones de pago

--- src/components/seccionProyectos/seccion5/tarifas/CardTarifas2.jsx
+++ src/components/seccionProyectos/seccion5/tarifas/CardTarifas2.jsx
@@ -1,32 +1,38 @@
-import React from 'react'
+
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCircleInfo, faCheck, faDollarSign  } from '@fortawesome/free-solid-svg-icons';
+import { faCircleInfo, faCheck, faDollarSign } from '@fortawesome/free-solid-svg-icons';
+import { MdStars } from "react-icons/md";
 const CardTarifas2 = ({opcion, precio,agregarCarrito}) => {
   return (
     <div className="ct_cardTarifas2">
-        <div className="ct_tituloYdesc">
-          <h2> {opcion} </h2>
-          <p>Viendo con detalle:
-            Incluye lo básico más una visualización realista del proyecto.
-          </p>
+        <div className="ct_cardTarifas2_div1">
+            <p>Más popular </p> <MdStars />
         </div>
-        <div className="ct_precio">
-          <span className="ct_signoDolar">
-            <FontAwesomeIcon icon={faDollarSign} style={{ color: 'var(--colorCheck)' }} />
-            <span className='ct_valor' > {precio} </span>
-          </span>
-        </div>
-        <div className="ct_btnCompra">
-          <button className="btn-comprar btn-comprarHover"onClick={()=>{agregarCarrito(precio,opcion)}} >Agregar Carrito</button>
-        </div>
-        <div className="ct_detalle">
-          <div className="ct_tituloEinfo">
-            <p>Planos con medidas</p>
-              <button>
-                <FontAwesomeIcon icon={faCircleInfo} style={{color: "var(--colorInfo)", fontSize: '18px'}} />
-              </button>
+
+        <div className="ct_cardTarifas2_div2">
+          <div className="ct_tituloYdesc">
+            <h2> {opcion} </h2>
+            <p>Viendo con detalle:
+              Incluye lo básico más una visualización realista del proyecto.
+            </p>
           </div>
-          
+          <div className="ct_precio">
+            <span className="ct_signoDolar">
+              <FontAwesomeIcon icon={faDollarSign} style={{ color: 'var(--colorCheck)' }} />
+              <span className='ct_valor' > {precio} </span>
+            </span>
+          </div>
+          <div className="ct_btnCompra">
+            <button className="btn-comprar btn-comprarHover"onClick={()=>{agregarCarrito(precio,opcion)}} >Agregar Carrito</button>
+          </div>
+          <div className="ct_detalle">
+            <div className="ct_tituloEinfo">
+              <p>Planos con medidas</p>
+                <button>
+                  <FontAwesomeIcon icon={faCircleInfo} style={{color: "var(--colorInfo)", fontSize: '18px'}} />
+                </button>
+          </div>
+              
           <ul>
             <li>
               <span className="icon-texto">
@@ -65,7 +71,7 @@ const CardTarifas2 = ({opcion, precio,agregarCarrito}) => {
                 <FontAwesomeIcon icon={faCircleInfo} style={{color: "var(--colorInfo)", fontSize: '18px'}} />
               </button>
           </div>
-          
+              
           <ul>
             <li>
               <span className="icon-texto">
@@ -98,6 +104,7 @@ const CardTarifas2 = ({opcion, precio,agregarCarrito}) => {
               </span>
             </li>
           </ul>
+          </div>
         </div>
     </div>
   )
